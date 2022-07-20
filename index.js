28f045c6c1f6cf6adbe43c14702e3ea4831383c7
Carlos Rangel <charlesleimaginant@gmail.com>
2022-07-19 21:38:57 -0500

    Add button to call the API ith the color and the mode

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const colorInput = document.querySelector('#color');
+const activate = document.getElementById('activate');
+const select = document.getElementById('mode');
+
+
+let color = colorInput.value;
+
+color = color.substring(1);
 /* const hex = document.querySelector('#hex'); */
 
 
-colorInput.addEventListener('input',()=>{
+/* colorInput.addEventListener('input',()=>{
     let color = colorInput.value;
     /* hex.style.color = color;
-    hex.innerHTML = color; */
+    hex.innerHTML = color;
     color = color.substring(1);
-    console.log(color);
-    fetch(`https://www.thecolorapi.com/scheme?hex=${color}`)
+    return color;
+}) */
+
+
+// Llama a la API
+function callApi(color, mode = "monochrome") {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}`)
         .then(response => response.json())
         .then(data => console.log(data))
-})
+}
+
+
+activate.addEventListener('click', function() {
+    const selectValue = select.options[select.selectedIndex].value;
+    callApi(color, selectValue);
+});
